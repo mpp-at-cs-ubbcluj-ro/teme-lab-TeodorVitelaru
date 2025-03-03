6fedf767338591d74c113e4f7987b2007e8485cb
Teodor Vitelaru <teodor_vitelaru@yahoo.com>
2025-03-03 15:43:47 +0200

    added gradle project

---
+++ IncercareGradle/src/test/java/TestTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+
+    @Test
+    @DisplayName("First Test")
+    public void firstTest() {
+        System.out.println("First Test");
+    }
+}
